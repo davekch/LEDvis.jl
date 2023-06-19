5ee33d312d83e5524354c9a863471682e8bce3a3
David Koch <dave-koch@web.de>
2023-06-19 18:55:16 +0200

    more examples

--- examples/growingcircle.jl
+++ examples/growingcircle.jl
@@ -10,7 +10,12 @@ layers = [
 
 for _ in 1:50
     animate!(layers)
-    asciivisualize(withlayout(evaluate(layers), layout))
+    asciivisualize(
+        withlayout(
+            evaluate(layers),
+            layout
+        )
+    )
     println()
     sleep(0.1)
 end
