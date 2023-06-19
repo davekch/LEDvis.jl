86ae0610e90d35ed6203e09f72128f50884a8843
David Koch <dave-koch@web.de>
2023-06-19 18:55:38 +0200

    add some layouts + handy constants

--- src/Ledvis.jl
+++ src/Ledvis.jl
@@ -1,7 +1,7 @@
 module Ledvis
 
 export Geometry, Layers, Animate, Visualize, LEDLayout
-export W, H, purple, shadow, bkgpurple, bkgshadow, layout
+export W, H, MX, MY, purple, green, shadow, bkgpurple, bkggreen, bkgshadow, layout
 
 include("Geometry.jl")
 include("Layers.jl")
@@ -9,15 +9,21 @@ include("Animate.jl")
 include("Visualize.jl")
 include("LEDLayout.jl")
 
-using .Layers
+using .Layers, .LEDLayout
 
 # convenience
-const W = H = 17
+const layout = LEDLayout.fromfile("resources/layout_medium.txt")
+const W = width(layout)
+const H = height(layout)
+# middle points
+const MX = W ÷ 2 + 1
+const MY = H ÷ 2 + 1
 const purple = Color(255, 0, 200)
+const green = Color(0, 255, 0)
 const shadow = Color(-255, -255, -255)
 const bkgpurple = monochromatic(purple, W, H)
+const bkggreen = monochromatic(green, W, H)
 const bkgshadow = monochromatic(shadow, W, H)
-const layout = LEDLayout.fromfile("resources/layout.txt")
 
 
 end # module ledvis
