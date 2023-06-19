76c192d74d73195e47e80877a31ae2cef1c4860c
David Koch <dave-koch@web.de>
2023-06-20 00:55:42 +0200

    add animated colors example

---
+++ examples/pulsate.jl
@@ -0,0 +1,21 @@
+using Ledvis
+using .Layers, .Visualize, .LEDLayout, .Animate
+
+pulse = pulsate(
+    t -> Fields.absexpfield(255, t) ∘ (p -> p - center),
+    t -> (_ -> 0),
+    t -> (_ -> 0),
+    0.05, 0.5, 0.05, W, H
+)
+
+for _ in 1:100
+    cmap = pulse()
+    asciivisualize(
+        withlayout(
+            evaluate([Layer([], cmap)]),
+            layout
+        )
+    )
+    println()
+    sleep(0.05)
+end
--- src/Animate.jl
+++ src/Animate.jl
@@ -1,6 +1,6 @@
 module Animate
 
-export animate!, linearmove, growcircle, rotate
+export animate!, linearmove, growcircle, rotate, pulsate
 using ..Geometry
 using ..Layers
 
@@ -58,6 +58,68 @@ end
 
 
 rotate(dphi) = rect::Rect -> setangle!(rect, Geometry.angle(rect) + dphi)
+
+
+"""
+    periodicparameter(f, minp, maxp, dp)
+
+return a function that calls `f(p)` with p periodically moving from `minp` to `maxp`
+and back, in steps of `dp`
+"""
+function periodicparameter(f, minp, maxp, dp)
+    p = minp
+    function _f()
+        result = f(p)
+        if p + dp > maxp
+            dp = -dp
+        elseif p + dp < minp
+            dp = -dp
+        end
+        p += dp
+        return result
+    end
+    _f
+end
+
+
+"""
+    pulsate(redf, bluef, greenf, minp, maxp, dp, W, H)
+
+returns a function that creates colorfields that vary periodically in time.  
+Example:
+
+    pulse = pulsate(
+        t -> Fields.absexpfield(255, t),
+        t -> (_ -> 0),
+        t -> (_ -> 0),
+        0.05, 0.5, 0.05, W, H
+    )
+    for _ in 1:20
+        cmap = pulse()   # red blob with varying size on each iteration
+        # ...
+    end
+
+Arguments:
+- `redf`, `bluef`, `greenf`: functions of type `p::Number -> (v::Vector -> Number)`.
+    the function should accept one parameter and return a function that turns a point into
+    a number. that number will be the value for the respective color.
+- `minp`, `maxp`, `dp`: on each successive call, the parameter `p` changes from `minp` to `maxp`
+    and back in steps of `dp`
+- `W`, `H`: the width and height of the generated colormap
+"""
+function pulsate(redf, bluef, greenf, minp, maxp, dp, W, H)
+    periodicred = periodicparameter(redf, minp, maxp, dp)
+    periodicgreen = periodicparameter(greenf, minp, maxp, dp)
+    periodicblue = periodicparameter(bluef, minp, maxp, dp)
+    function pulsatingcolorfield()
+        red = field(periodicred(), W, H)
+        green = field(periodicgreen(), W, H)
+        blue = field(periodicblue(), W, H)
+        return colorfield(red, green, blue)
+    end
+    pulsatingcolorfield
+end
+
 
 
 end # module Animate
