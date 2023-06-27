c5841ee7acf6aca03fd63005234877221130590e
davekch <dave-koch@web.de>
2023-06-27 22:07:01 +0200

    serialize to bytes instead of strings

--- scripts/termvisual.jl
+++ scripts/termvisual.jl
@@ -6,7 +6,7 @@ using Sockets
 socket = connect(2002)
 while isopen(socket)
     @info "waiting to read"
-    s = readline(socket)
+    s = read(socket, 540)
     @info "read message, visualizing:"
     cmap = deserialize(s, Ledvis.layout)
     asciivisualize(cmap)
--- src/Serial.jl
+++ src/Serial.jl
@@ -13,20 +13,18 @@ function serialize(colormap, layout::Layout)
     for (j, i) in indices(layout)
         push!(ser, colormap[j, i]...)
     end
-    join(ser, ",")
+    map(UInt8, ser)
 end
 
 
 function deserialize(msg, layout::Layout)
-    ints = map(x -> parse(Int64, x), split(msg, ','))
     cmap = Array{Any}(missing, height(layout), width(layout))
     for (i, (y, x)) in enumerate(indices(layout))
         imin = (i - 1) * 3 + 1
         imax = (i - 1) * 3 + 3
-        cmap[y, x] = tuple(ints[imin:imax]...)
+        cmap[y, x] = tuple(msg[imin:imax]...)
     end
     cmap
-    ser
 end
 
 
