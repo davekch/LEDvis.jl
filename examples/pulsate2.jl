1607cb6f14de5c5cd5e48d653f0d6b49d9183d01
davekch <dave-koch@web.de>
2023-07-24 20:13:41 +0200

    improve example pulsate2

--- examples/pulsate2.jl
+++ examples/pulsate2.jl
@@ -2,12 +2,13 @@ using Ledvis
 using .Animate, .Visualize, .Geometry, .LEDLayout, .Layers, .Serial
 using LibSerialPort
 
-f = Animate.periodicparameter(identity, 1, 300, 15)
-g = Animate.periodicparameter(identity, 1, 300, 11)
-circle = Circle(1, center)
+f = Animate.periodicparameter(identity, 1, 300, 5)
+g = Animate.periodicparameter(identity, 1, 300, 3)
+circle1 = Circle(1, center - [18, 0])
+circle2 = Circle(1, center + [18, 0])
 layers = [
-    Layer(Dict(Glow(circle, 1) => glow -> glow.t = f()), bkggreen),
-    Layer(Dict(Glow(circle, 1) => glow -> glow.t = g()), bkgpurple)
+    Layer(Dict(Glow(circle1, 1) => glow -> glow.t = f()), monochromatic(Color(180, 0, 100), W, H)),
+    Layer(Dict(Glow(circle2, 1) => glow -> glow.t = g()), monochromatic(Color(0, 40, 150), W, H))
 ]
 
-clock = Clock(80, 8)
+clock = Clock(120, 8)
